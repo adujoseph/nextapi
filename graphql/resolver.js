6a33fa019fd42932db7c91676178c61fc02b7aa9
adujoseph <iamjosephadu@gmail.com>
2020-06-24 18:23:28 +0100

    Update resolver.js

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,11 +1,1 @@
-const User = require('../models/User');
-const Search = require('../models/Search');
 
-module.exports = {
-    hello(){
-        return {
-           text: 'Hello Word',
-           views: 123456
-        }
-    }
-}
